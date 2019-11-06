77ca78df3816d85dd0a46fcbe3d6ca0e0054dfcb
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2019-11-06 20:19:27 +0000

    Automatically backed up by Learn

--- db/migrate/20191106201802_create_users_table.rb
+++ db/migrate/20191106201802_create_users_table.rb
@@ -1,10 +1,11 @@
 class CreateUsersTable < ActiveRecord::Migration[5.1]
+
   def change
-    create_table :users do |t| 
+    create_table :users do |t|
       t.string :username
       t.string :password
       t.decimal :balance
     end
   end
-  
+
 end
